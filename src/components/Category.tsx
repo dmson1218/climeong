45ca740f2c9d97b3b28d46409b4e123d2cb095bb
dmson1218 <dmson1218@naver.com>
2025-02-14 15:27:34 +0900

    chore: 변수명 수정

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,11 +1,11 @@
 import Button from "./Button";
 
-const titles: string[] = ["클라이밍 뉴스", "장비 정보", "커뮤니티", "크루 홍보"];
+const CategoryTitles: string[] = ["클라이밍 뉴스", "장비 정보", "커뮤니티", "크루 홍보"];
 
 const Category: React.FC = () => {
     return (
         <div className="w-full flex-center grow border-b border-gray-300">
-            {titles.map((title) => (
+            {CategoryTitles.map((title) => (
                 <Button key={title} title={title} />
             ))}
         </div>
