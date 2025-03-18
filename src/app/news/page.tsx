e84ae271b2792c28443feb1c1ceb333e88b4b54f
dmson1218 <dmson1218@naver.com>
2025-03-18 18:16:29 +0900

    feat: 동적 화면 높이에 따른 게시물 개수 조정 기능 구현

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -7,13 +7,32 @@ import Link from "next/link";
 
 export default function Home() {
     const [news, setNews] = useState<Post[]>([]);
+    const [postsToShow, setPostsToShow] = useState<number>(10);
+
+    const updatePostsToShow = () => {
+        const height = window.innerHeight;
+        if (height >= 800) {
+            setPostsToShow(10);
+        } else if (height >= 600) {
+            setPostsToShow(7);
+        } else {
+            setPostsToShow(5);
+        }
+    };
 
     useEffect(() => {
-        fetch("/api/posts")
+        fetch(`/api/posts?count=10`)
             .then((res) => res.json())
             .then((data) => {
                 setNews(data);
             });
+
+        updatePostsToShow();
+        window.addEventListener("resize", updatePostsToShow);
+
+        return () => {
+            window.removeEventListener("resize", updatePostsToShow);
+        };
     }, []);
 
     return (
@@ -21,12 +40,11 @@ export default function Home() {
             <BoardWrapper>
                 <div className="my-3 flex-center text-xl mb-4">최신 소식</div>
                 <div className="grow flex flex-col gap-1">
-                    {news.length > 0 &&
-                        news.map((post) => (
-                            <div key={post._id} className="flex-center">
-                                {post.title}
-                            </div>
-                        ))}
+                    {news.slice(0, postsToShow).map((post) => (
+                        <div key={post._id} className="flex-center">
+                            {post.title}
+                        </div>
+                    ))}
                 </div>
                 <div className="flex justify-end">
                     <Link href="/news/create">글쓰기</Link>
