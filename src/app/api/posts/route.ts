99ace6c78f42bf253d26589da340e82636d200f1
dmson1218 <dmson1218@naver.com>
2025-03-18 16:36:20 +0900

    refactor: MongoDB 클라이언트 연결 로직 분리

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,32 +1,9 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { getClient } from "@/database/dbClient";
 
 export interface Post {
     _id: string;
     title: string;
     content: string;
-}
-
-const uri = process.env.MONGODB_URI;
-if (!uri) {
-    throw new Error("MONGODB_URI가 정의되지 않았습니다.");
-}
-
-let client: MongoClient | null = null;
-let clientPromise: Promise<MongoClient> | null = null;
-
-async function getClient() {
-    if (!client) {
-        client = new MongoClient(uri as string, {
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
-            },
-        });
-        clientPromise = client.connect();
-    }
-
-    return clientPromise!;
 }
 
 export async function GET(request: Request) {
---
+++ src/database/dbClient.ts
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+    throw new Error("MONGODB_URI가 정의되지 않았습니다.");
+}
+
+let client: MongoClient | null = null;
+let clientPromise: Promise<MongoClient> | null = null;
+
+async function getClient() {
+    if (!client) {
+        client = new MongoClient(uri as string, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            },
+        });
+        clientPromise = client.connect();
+    }
+
+    return clientPromise!;
+}
+
+export { getClient };
