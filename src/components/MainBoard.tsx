e6a5517626a0de64db054f431d146de8f7fb9824
dmson1218 <dmson1218@naver.com>
2025-03-18 17:38:52 +0900

    style: 반응형 디자인 수정

--- src/components/MainBoard.tsx
+++ src/components/MainBoard.tsx
@@ -52,7 +52,7 @@ const MainBoard = () => {
 
 const BoardWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className="grow px-8 py-4 m-2 rounded border-2 border-slate-300 flex flex-col">
+        <div className="grow px-6 py-4 m-2 rounded border-2 border-slate-300 flex flex-col">
             {children}
         </div>
     );
