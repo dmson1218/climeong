edb2309214c66ed9f35d875c7bbb3935b772d6ae
dmson1218 <dmson1218@naver.com>
2025-03-18 17:17:46 +0900

    style: 반응형 디자인 수정

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { Post } from "../api/posts/route";
 import { BoardWrapper } from "@/components/MainBoard";
+import Link from "next/link";
 
 export default function Home() {
     const [news, setNews] = useState<Post[]>([]);
@@ -16,7 +17,7 @@ export default function Home() {
     }, []);
 
     return (
-        <div className="h-[calc(100vh-5rem-60px)] grid z-10 px-4">
+        <div className="min-h-[calc(100vh-5rem-60px)] grid z-10 px-4">
             <BoardWrapper>
                 <div className="mt-3 mb-8 flex-center text-xl mb-4">최신 소식</div>
                 <div className="grow flex flex-col gap-1">
@@ -28,7 +29,7 @@ export default function Home() {
                         ))}
                 </div>
                 <div className="flex justify-end">
-                    <button>더보기</button>
+                    <Link href="/news/create">글쓰기</Link>
                 </div>
             </BoardWrapper>
         </div>
--- src/app/rope/page.tsx
+++ src/app/rope/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <div className="h-[calc(100vh-5rem-60px)] grid z-10"></div>;
+    return <div className="min-h-[calc(100vh-5rem-60px)] grid z-10"></div>;
 }
