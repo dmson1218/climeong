5f84d9161c7848664cf9e55dff6be5fecda37566
dmson1218 <dmson1218@naver.com>
2025-03-24 16:19:12 +0900

    feat: 줄바꿈 적용을 위한 FormatContent 컴포넌트 구현

---
+++ src/components/FormatContent.tsx
@@ -0,0 +1,18 @@
+interface FormatContentProps {
+    content: string;
+}
+
+const FormatContent: React.FC<FormatContentProps> = ({ content }) => {
+    const formattedContent = content.split("\n").map((line, index) => {
+        return (
+            <span key={index}>
+                {line}
+                <br />
+            </span>
+        );
+    });
+
+    return <>{formattedContent}</>;
+};
+
+export { FormatContent };
