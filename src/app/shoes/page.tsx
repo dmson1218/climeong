ef54318ba8b83e7db6ff46f622dd2386390e9287
dmson1218 <dmson1218@naver.com>
2025-03-24 15:15:50 +0900

    feat: 각 페이지 기본 구조 설정

--- src/app/shoes/page.tsx
+++ src/app/shoes/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import { BoardWrapper } from "@/components/MainBoard";
+
 export default function ShoesPage() {
-    return <div className="min-h-[calc(100vh-5rem-60px)] grid z-10"></div>;
+    return (
+        <div className="min-h-[calc(100vh-5rem-60px)] mt-20 grid px-4">
+            <BoardWrapper>
+                <div className="my-3 flex-center text-xl mb-4">암벽화 정보</div>
+                <div className="grow flex flex-col gap-1"></div>
+                <div className="flex justify-end">
+                    <Link href="/news/create">글쓰기</Link>
+                </div>
+            </BoardWrapper>
+        </div>
+    );
 }
