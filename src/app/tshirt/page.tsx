9555e19685bdd08f6452c9984fb753832bb2c8d2
dmson1218 <dmson1218@naver.com>
2025-03-28 15:03:15 +0900

    feat: 초크 및 티셔츠 페이지 기본 구조 구현

---
+++ src/app/tshirt/page.tsx
@@ -0,0 +1,12 @@
+import { BoardWrapper } from "@/components/MainBoard";
+
+export default function TshirtPage() {
+    return (
+        <div className="min-h-[calc(100vh-5rem-60px)] mt-20 grid px-4">
+            <BoardWrapper>
+                <div className="my-3 flex-center text-xl mb-4">티셔츠 정보</div>
+                <div className="grow flex-center flex-wrap gap-1"></div>
+            </BoardWrapper>
+        </div>
+    );
+}
