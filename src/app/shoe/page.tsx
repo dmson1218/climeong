9a6e85774866c75d796840b7a5fc189c1ccdbf6d
dmson1218 <dmson1218@naver.com>
2025-08-01 15:31:31 +0900

    style: 반응형 디자인 수정

--- src/app/shoe/page.tsx
+++ src/app/shoe/page.tsx
@@ -1,5 +1,6 @@
 import BoardWrapper from "@/components/Board/BoardWrapper";
-import Shoe from "@/components/Shoe";
+import Image from "next/image";
+import Link from "next/link";
 
 const shoeNames = [
   "Butora_NewComet",
@@ -29,10 +30,33 @@ export default function ShoePage() {
   return (
     <div className="layout mt-24 grid min-h-[calc(100vh-6rem-60px)]">
       <BoardWrapper>
-        <div className="flex-center my-3 mb-4 text-xl">암벽화 정보</div>
-        <div className="mx-16 grid grow grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4">
+        <div className="mx-3 flex flex-wrap items-center md:mx-0">
           {shoeNames.map((shoeName) => (
-            <Shoe key={shoeName} shoeName={shoeName} />
+            <div
+              key={shoeName}
+              className="w-1/2 flex-shrink-0 p-1 sm:w-1/3 lg:w-1/4"
+            >
+              <Link
+                href=""
+                className="relative flex aspect-square flex-col items-center overflow-hidden rounded-lg border border-gray-200"
+              >
+                <Image
+                  src={`/images/shoes/${shoeName}.jpg`}
+                  alt={shoeName}
+                  width={192}
+                  height={192}
+                  className="w-full object-cover p-2 transition-transform duration-300 ease-in-out hover:scale-105"
+                />
+                <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/20 to-transparent px-2 py-1 text-left text-xs">
+                  <div className="font-normal italic">
+                    # {shoeName.split("_")[0]}
+                  </div>
+                  <div className="font-normal italic">
+                    # {shoeName.split("_")[1]}
+                  </div>
+                </div>
+              </Link>
+            </div>
           ))}
         </div>
       </BoardWrapper>
--- src/components/Shoe.tsx
+++ src/components/Shoe.tsx
@@ -5,21 +5,24 @@ const Shoe = ({ shoeName }: { shoeName: string }) => {
   const [brand, model] = shoeName.split("_");
 
   return (
-    <Link href="" className="flex grow flex-col items-center">
-      <div className="relative aspect-square w-36 overflow-hidden rounded-lg border border-gray-200 md:w-48">
+    <div className="flex grow flex-col items-center">
+      <Link
+        href={`shoe/${shoeName}`}
+        className="relative aspect-square w-36 overflow-hidden rounded-lg border border-gray-200 md:w-48"
+      >
         <Image
           src={`/images/shoes/${shoeName}.jpg`}
           alt={shoeName}
           width={192}
           height={192}
-          className="object-cover"
+          className="w-full object-cover p-2"
         />
         <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/20 to-transparent px-2 py-1 text-left text-xs">
           <div className="font-normal italic"># {brand}</div>
           <div className="font-normal italic"># {model}</div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
