82411f17feb389c577edd0302da1cc8ee3ac0da7
dmson1218 <dmson1218@naver.com>
2025-08-21 17:05:36 +0900

    feat: 메타 데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Next.js App",
-  description: "My Next.js App Description",
+  title: "Climeong",
+  description: "클라이머의, 클라이머에 의한, 클라이머를 위한",
 };
 
 export default function RootLayout({
